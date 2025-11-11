38b0c2d37e4c6da79d6bd28dab576835803c360d
Anjali yadav <anjaliyadav08905@gmail.com>
2025-11-11 05:54:56 +0530

    Merge branch 'main' of https://github.com/anjali0388/My-intership

---
+++ week2/day1/this.js
@@ -0,0 +1,10 @@
+// variable scopping & this 
+// let name ="anjali";
+// function greet()
+// {
+//     let name=  "muskan"
+//     console.log("inside function ",name)
+// }
+// greet()
+// console.log("outside the function")
+console.log("hello i am anjali")
